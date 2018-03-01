74f1e28ba1f216d302a8cb6f30a3447549d205d5
Sunny Shek <azala.justice@gmail.com>
2018-03-01 09:38:05 +0000

    Lab 9

--- Lab9/contentlist.js
+++ Lab9/contentlist.js
@@ -7,8 +7,8 @@ $(function(){
   // 2. add an event listener for 'click' events
 	// 3. assign the addContent() function to the click event
 	$('#addcontent').click(function addContent(){
-		$("#div").append(items_html)
-	})
+
+	});
 });
 
 function addContent () {
@@ -26,6 +26,6 @@ function addContent () {
 	// using jQuery
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
-	$("#div").append(items_html);
+	$("#content").append(items_html);
 
 }
