ac6fd98240ff308e60967da00d337421407918d2
Sunny Shek <azala.justice@gmail.com>
2018-03-01 09:29:36 +0000

    Lab 9

--- Lab9/contentlist.js
+++ Lab9/contentlist.js
@@ -6,7 +6,7 @@ $(function(){
 	// 1. find the addcontent button in task-two.html
   // 2. add an event listener for 'click' events
 	// 3. assign the addContent() function to the click event
-	$('#addcontent').click(function(){
+	$('#addcontent').click(function addContent(){
 		$("#div").append(items_html)
 	})
 });
